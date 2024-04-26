551f1016542df61fe202f292d27ccc4c99d844e4
David Vitas <david.vitas@hotmail.com>
2024-04-26 19:23:40 +0200

    adding header

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import Header from "./components/header";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image source={require("./assets/adaptive-icon.png")} />
+      <Header />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#252121",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
