860b8620c9cbe306461aecfa23d2aeabc5743d6c
OMaddams <Omaddams@hotmail.com>
2024-04-26 18:40:02 +0200

    basic styling of todo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import Todo from "./components/todo";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Image source={require("./assets/adaptive-icon.png")} />
+
+      <Todo />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+const todo = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HELLO</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#3A3737",
+    color: "#BD8904",
+  },
+});
+
+export default todo;
