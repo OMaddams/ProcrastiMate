c3b8b6a08f3903d09182549499b1825bc4faf059
OMaddams <Omaddams@hotmail.com>
2024-04-26 21:26:46 +0200

    merge fix

--- components/todo.js
+++ components/todo.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import {
-
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
+import {
   useFonts,
   Inter_900Black,
   Inter_500Medium,
