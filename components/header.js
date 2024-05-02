06d04721214e615b831c16f647cc77ecfab2c320
OMaddams <Omaddams@hotmail.com>
2024-05-02 15:30:05 +0200

    settings and logo color change

--- components/header.js
+++ components/header.js
@@ -5,17 +5,23 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
+import Logo from "../assets/Logo.js";
+import SettingsIcon from "../assets/SettingsIcon.js";
 const header = ({ themeColor }) => {
   return (
     <View style={[styles.container, { borderBottomColor: themeColor }]}>
       <View style={styles.logoContainer}>
-        <Image source={require("../assets/Logo.svg")} style={styles.image} />
+        {/* <Image source={require("../assets/Logo.svg")} style={styles.image} /> */}
+        <Logo containerStyle={styles.image} themeColor={themeColor} />
       </View>
-      <Image
+      <SettingsIcon
+        themeColor={themeColor}
+        containerStyle={styles.settingsIcon}
+      />
+      {/* <Image
         source={require("../assets/settingsIcon.svg")}
         style={styles.settingsIcon}
-      />
+      /> */}
     </View>
   );
 };
