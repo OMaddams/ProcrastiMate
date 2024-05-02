ebb010721b6923b256782019dc93940cd1222d1c
OMaddams <Omaddams@hotmail.com>
2024-05-02 16:33:45 +0200

    completeCheck Icon

---
+++ assets/completeCheck.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Svg, { Circle } from "react-native-svg";
+const SvgComponent = ({ themeColor, containerStyle }) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={containerStyle.width}
+    height={containerStyle.height}
+    fill="none"
+    style={containerStyle}
+  >
+    <Circle
+      cx={16.5}
+      cy={16.5}
+      r={15.5}
+      fill={themeColor}
+      stroke={themeColor}
+      strokeWidth={2}
+    />
+  </Svg>
+);
